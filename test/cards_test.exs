38d1fa8f4c872933497367df5e176944fc38fe5a
Roy Wei <roywjy@gmail.com>
2024-07-06 14:06:27 +1000

    Add some tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -5,4 +5,31 @@ defmodule CardsTest do
   test "greets the world" do
     assert Cards.hello() == :world
   end
+
+  test "creates a deck of cards" do
+    deck = Cards.create_deck()
+    assert length(deck) == 52
+    assert Cards.contains?(deck, "Ace of Hearts")
+    assert Cards.contains?(deck, "King of Spades")
+  end
+
+  test "shuffles a deck of cards" do
+    deck = Cards.create_deck()
+    shuffled_deck = Cards.shuffle(deck)
+    refute deck == shuffled_deck
+  end
+
+  test "deals a hand of cards" do
+    deck = Cards.create_deck()
+    {hand, remaining} = Cards.deal(deck, 5)
+    assert length(hand) == 5
+    assert length(remaining) == 47
+  end
+
+  test "deals a hand of cards with a different hand size" do
+    deck = Cards.create_deck()
+    {hand, remaining} = Cards.deal(deck, 10)
+    assert length(hand) == 10
+    assert length(remaining) == 42
+  end
 end
