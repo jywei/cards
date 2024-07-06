76556d88035ff241cdb98c63ca73379579053f5d
Roy Wei <roywjy@gmail.com>
2024-07-06 14:02:10 +1000

    Refactor deal function in Cards module and update documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -51,6 +51,15 @@ defmodule Cards do
 
   @doc """
     Returns a tuple with the first element being the hand and the second element being the remaining deck.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> {hand, remaining} = Cards.deal(deck, 5)
+      iex> hand
+      ["Ace of Hearts", "Two of Hearts", "Three of Hearts", "Four of Hearts", "Five of Hearts"]
+      iex> remaining
+      ["Six of Hearts", "Seven of Hearts", "Eight of Hearts", "Nine of Hearts", "Ten of Hearts", "Jack of Hearts", "Queen of Hearts", "King of Hearts", "Ace of Diamonds", "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds", "Six of Diamonds", "Seven of Diamonds", "Eight of Diamonds", "Nine of Diamonds", "Ten of Diamonds", "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds", "Ace of Clubs", "Two of Clubs", "Three of Clubs", "Four of Clubs", "Five of Clubs", "Six of Clubs", "Seven of Clubs", "Eight of Clubs", "Nine of Clubs", "Ten of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs", "Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades", "Five of Spades", "Six of Spades", "Seven of Spades", "Eight of Spades", "Nine of Spades", "Ten of Spades", "Jack of Spades", "Queen of Spades", "King of Spades"]
   """
   def deal(deck, n) do
     {hand, remaining} = Enum.split(deck, n)
